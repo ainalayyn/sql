5ff07ede7e9d4e4c5d9951c16b58b313b59f0393
Tomiris Amangeldi <90600383+ainalayyn@users.noreply.github.com>
2021-12-12 17:43:29 +0600

    Add files via upload

---
+++ 1 практическое задание.sql
@@ -0,0 +1,7 @@
+create database TaRis;
+create table articles(
+    id int not null
+    auto_increment,
+    name varchar(20)
+    primary key(id)
+);
---
+++ практическое занятие 2.sql
@@ -0,0 +1,4 @@
+insert into articles (title, views, text, pub_date) values
+("Google",402,"Google очень большая компания","2020-01-04"),
+("FaceBook",899,"FaceBook очень большая компания","2020-04-11"),
+("Apple",288,"Apple очень большая компания","2020-02-12");
